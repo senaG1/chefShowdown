344b33bd711230ad2434acc21660bcdc401406a3
33de9r <chenjiayu2033@gmail.com>
2025-11-04 21:28:23 -0500

    Changed Effect class to extend from SuperSmoothMover

--- Effect.java
+++ Effect.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Effect extends Actor
+public class Effect extends SuperSmoothMover
 {
     /**
      * Act - do whatever the Effect wants to do. This method is called whenever
