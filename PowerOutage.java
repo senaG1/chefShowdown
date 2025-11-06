7daab0578358e86c3647fb48282689b4267ed25c
33de9r <chenjiayu2033@gmail.com>
2025-11-06 10:17:35 -0500

    Created Skeleton Classes

---
+++ PowerOutage.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class PowerOutage here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PowerOutage extends Effect
+{
+    /**
+     * Act - do whatever the PowerOutage wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
