0ba0eac4e0bb55d5ccc51a236d9cc4941f9eeec5
caydenchan68 <caydenchan66@gmail.com>
2025-11-06 10:12:42 -0500

    Skeleton of Chef class + subclasses

---
+++ MasterChef.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MasterChef here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MasterChef extends Chef
+{
+    /**
+     * Act - do whatever the MasterChef wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
