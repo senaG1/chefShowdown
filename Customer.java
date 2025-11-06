f816c19caf067c14ac70572c170a286d1dbe79fb
oscarh414 <oscarh091008@gmail.com>
2025-11-06 10:41:02 -0500

    lineUp() method in Customer

--- Customer.java
+++ Customer.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.ArrayList;
 /**
  * Write a description of class Customer here.
  * 
@@ -8,17 +8,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Customer extends SuperSmoothMover
 {
+    private int timeSpent;
     /**
      * Act - do whatever the Customer wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        // Add your action code here.
+        
+    }
+    
+    public Customer()
+    {
+        timeSpent = 0;
     }
     
     public void lineUp()
     {
+        ArrayList<Customer> totalCustomers = (ArrayList<Customer>)getWorld().getObjects(Customer.class);
         
     }
 }
