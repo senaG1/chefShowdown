b3ccafef44dbecb0fcae1f9ae296d5e5d39ff049
isabelpowell57-cell <isabelpowell57@gmail.com>
2025-11-06 09:41:02 -0500

    Added classes

---
+++ RegularCustomer.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class RegularCustomer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class RegularCustomer extends Customer
+{
+    /**
+     * Act - do whatever the RegularCustomer wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
