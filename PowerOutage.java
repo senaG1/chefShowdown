db205423833c789a6e34c78a86959b5c9646cca8
senaG1 <161520172+senaG1@users.noreply.github.com>
2025-11-06 22:04:31 -0500

    I added all the code we need for the Effects subclass and added the base cade for the subclasses of the Effect class

--- PowerOutage.java
+++ PowerOutage.java
@@ -8,10 +8,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class PowerOutage extends Effect
 {
-    /**
-     * Act - do whatever the PowerOutage wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+     public PowerOutage()
+    {
+        super(300); 
+    }
+    
+    public void loseCustomers()
+    {
+        // Empty for now
+    }
+    
     public void act()
     {
         // Add your action code here.
--- RatInfestation.java
+++ RatInfestation.java
@@ -8,10 +8,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class RatInfestation extends Effect
 {
-    /**
-     * Act - do whatever the RatInfestation wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    public RatInfestation()
+    {
+        super(600); 
+    }
+    
+    public void loseCustomers()
+    {
+        // Empty for now - you'll fill this in later
+    }
+    
     public void act()
     {
         // Add your action code here.
