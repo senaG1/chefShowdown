f7b6a2d4b47ddcf29f9e755ace2d98cc39c53ee8
senaG1 <161520172+senaG1@users.noreply.github.com>
2025-11-07 10:11:05 -0500

    add black translucent image to power outage class

--- PowerOutage.java
+++ PowerOutage.java
@@ -8,9 +8,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class PowerOutage extends Effect
 {
-     public PowerOutage()
+    private int actCount;
+    private int totalFadeTime;
+    
+    public PowerOutage()
     {
         super(300); 
+        drawimage();
+        actCount = 240;
+        totalFadeTime = 90;
     }
     
     public void loseCustomers()
@@ -21,5 +27,12 @@ public class PowerOutage extends Effect
     public void act()
     {
         // Add your action code here.
+    }
+    
+    private void drawimage() {
+        image = new GreenfootImage(2048, 800);
+        image.setColor(new Color(0, 0, 40, 80));
+        image.fill();
+        setImage(image);
     }
 }
