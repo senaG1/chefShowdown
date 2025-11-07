e721ae3d14a45a63cb775ca62fa4632ff145a538
33de9r <chenjiayu2033@gmail.com>
2025-11-06 22:28:39 -0500

    Added labels and cash icon to UI bar

--- UI.java
+++ UI.java
@@ -1,19 +1,67 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class UI here.
+ * User Interface that shows the viewers the current ratings of each restaurant
+ * and how much cash they have.
  * 
- * @author Jiayu 
- * @version 1.1
+ * The rating counter will show a double from 1-5 accompanied with star icons.
+ * 
+ * The cash counter will update the amount of cash each team has.
+ * 
+ * @author Jiayu Chen
+ * @version 11-6
  */
 public class UI extends SuperSmoothMover
 {
-    GreenfootImage ui = new GreenfootImage("UI.png");
-    public UI() {
+    private static int labelHeight = 38;
+    private static int labelSize = 25;
+    private GreenfootImage ui = new GreenfootImage("UI.png");
+    
+    private Label cashLabelA;
+    private Label ratingLabelA;
+    private Label cashLabelB;
+    private Label ratingLabelB;
+    
+    private UI_Image cashIconA;
+    private UI_Image cashIconB;
+    
+    // initially each restaurant starts with $1000 to hire chefs and price their menus etc.
+    public int cashCounter = 1000;
+    
+    // initially each restaurant has a rating of 0 because they just opened.
+    public double starRating = 0;
+    
+    public UI(World world) {
         setImage(ui);
+        world.addObject(this, 960, 40);
+        
+        GreenfootImage cashImage = new GreenfootImage("cash.png");
+        cashIconA = new UI_Image(cashImage);
+        cashIconB = new UI_Image(cashImage);
+        
+        // labels for the UI
+        cashLabelA = new Label("CASH: ", labelSize);
+        ratingLabelA = new Label("RATING: ", labelSize);
+        cashLabelB = new Label("CASH: ", labelSize);
+        ratingLabelB = new Label("RATING: ", labelSize);
+        
+        // add labels and images to UI
+        addImages(world);
     }
     
     public void act() {
         // Add your action code here.
+    }
+    
+    private void addImages(World w) {
+        // blue team
+        w.addObject(cashLabelA, 310, labelHeight);
+        w.addObject(cashIconA, 450, labelHeight);
+        w.addObject(ratingLabelA, 60, labelHeight);
+        
+        // red team
+        w.addObject(cashLabelB, 780, labelHeight);
+        w.addObject(cashIconB, 920, labelHeight);
+        w.addObject(ratingLabelB, 530, labelHeight);
     }
 }
