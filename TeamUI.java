725b03e01d7f16e9ceba882ddfaf4a09c5dd276a
33de9r <chenjiayu2033@gmail.com>
2025-11-11 18:56:20 -0500

    factored code for UI and added TeamUI subclass

---
+++ TeamUI.java
@@ -0,0 +1,59 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TeamUI here.
+ * 
+ * @author Jiayu Chen
+ * @version 11-10
+ */
+public class TeamUI {
+    private Label cashLabel;
+    private Label cashCounter;
+    private Label ratingLabel;
+    private UI_Image cashIcon;
+    private UI_Image starIcon;
+
+    private int cash;
+    private double rating;
+
+    public TeamUI(World world, int xOffset, int labelHeight, int labelSize, String teamColor) {
+        cash = 1000;
+        rating = 0;
+
+        cashLabel = new Label("CASH: ", labelSize);
+        cashCounter = new Label(cash, labelSize);
+        ratingLabel = new Label("RATING: ", labelSize);
+
+        GreenfootImage cashImage = new GreenfootImage("cash.png");
+        cashIcon = new UI_Image(cashImage);
+
+        GreenfootImage starImage = new GreenfootImage("star_rating/stars11.png");
+        starImage.scale(190, 40);
+        starIcon = new UI_Image(starImage);
+
+        // Positioning
+        world.addObject(cashLabel, xOffset, labelHeight);
+        world.addObject(cashCounter, xOffset + 70, labelHeight);
+        world.addObject(cashIcon, xOffset + 110, labelHeight);
+        world.addObject(ratingLabel, xOffset - 280, labelHeight);
+        world.addObject(starIcon, xOffset - 150, labelHeight - 5);
+    }
+
+    public void updateCash(int newCash) {
+        cash = newCash;
+        cashCounter.setValue(cash);
+    }
+
+    public void updateRating(double newRating) {
+        rating = newRating;
+        //update star images based on rating
+    }
+
+    public int getCash() {
+        return cash;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+}
--- UI.java
+++ UI.java
@@ -18,17 +18,8 @@ public class UI extends SuperSmoothMover
     private static int labelSize = 25;
     private GreenfootImage ui = new GreenfootImage("UI.png");
     
-    private Label cashLabelA;
-    private Label cashCounterA;
-    private Label ratingLabelA;
-    private Label cashLabelB;
-    private Label cashCounterB;
-    private Label ratingLabelB;
-    
-    private UI_Image cashIconA;
-    private UI_Image cashIconB;
-    private UI_Image starIconA;
-    private UI_Image starIconB;
+    private TeamUI teamRed;
+    private TeamUI teamBlue;
     
     // initially each restaurant starts with $1000 to hire chefs and price their menus etc.
     public int cashCounter = 1000;
@@ -45,44 +36,11 @@ public class UI extends SuperSmoothMover
         setImage(ui);
         world.addObject(this, 960, 40);
         
-        GreenfootImage cashImage = new GreenfootImage("cash.png");
-        cashIconA = new UI_Image(cashImage);
-        cashIconB = new UI_Image(cashImage);
-        
-        GreenfootImage starImage = new GreenfootImage("star_rating/stars11.png");
-        starImage.scale(190, 40);
-        starIconA = new UI_Image(starImage);
-        starIconB = new UI_Image(starImage);
-        
-        // labels for the UI
-        cashLabelA = new Label("CASH: ", labelSize);
-        cashCounterA = new Label(cashCounter, labelSize);
-        ratingLabelA = new Label("RATING: ", labelSize);
-        cashLabelB = new Label("CASH: ", labelSize);
-        cashCounterB = new Label(cashCounter, labelSize);
-        ratingLabelB = new Label("RATING: ", labelSize);
-        
-        // add labels and images to UI
-        addImages(world);
+        teamRed = new TeamUI(world, 340, labelHeight, labelSize, "red");
+        teamBlue = new TeamUI(world, 810, labelHeight, labelSize, "blue");
     }
     
     public void act() {
         // Add your action code here.
-    }
-    
-    private void addImages(World w) {
-        // blue team
-        w.addObject(cashLabelA, 340, labelHeight);
-        w.addObject(cashCounterA, 410, labelHeight);
-        w.addObject(cashIconA, 450, labelHeight);
-        w.addObject(ratingLabelA, 60, labelHeight);
-        w.addObject(starIconA, 190, labelHeight - 5);
-        
-        // red team
-        w.addObject(cashLabelB, 810, labelHeight);
-        w.addObject(cashCounterB, 880, labelHeight);
-        w.addObject(cashIconB, 920, labelHeight);
-        w.addObject(ratingLabelB, 530, labelHeight);
-        w.addObject(starIconB, 660, labelHeight - 5);
     }
 }
