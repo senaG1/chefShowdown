7b03c2eb6686b72a6b44d1f9ecf72b1d99b11f1f
gremoce <grcttwl@gmail.com>
2025-11-14 09:38:11 -0500

    settings world

--- SettingsWorld.java
+++ SettingsWorld.java
@@ -8,22 +8,210 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class SettingsWorld extends World
 {
-    private int numChefs;
-    private int money;
+    // Blue restaurant settings
+    private int numChefsBlue = 1;
+    private int startMoneyBlue = 1000;
+    
+    // Red restaurant settings
+    private int numChefsRed = 1;
+    private int startMoneyRed = 1000;
+    
+    private GreenfootImage blueMenu;
+    private GreenfootImage redMenu;
+    
     private GreenfootImage background;
+    
+    // Blue restaurant buttons
+    private Button chefPlusBlue;
+    private Button chefMinusBlue;
+    private Button moneyPlusBlue;
+    private Button moneyMinusBlue;
+    
+    // Red restaurant buttons
+    private Button chefPlusRed;
+    private Button chefMinusRed;
+    private Button moneyPlusRed;
+    private Button moneyMinusRed;
+    
+    private Button playBtn;
+    
+    private final int MIN_CHEFS = 1;
+    private final int MAX_CHEFS = 5;
+    private final int MIN_MONEY = 500;
+    private final int MAX_MONEY = 5000;
+    private final int MONEY_INCREMENT = 100;
+    
+    private final int BLUE_CENTER_X = 280;
+    private final int BLUE_CHEF_Y = 270;
+    private final int BLUE_MONEY_Y = 340;
+    
+    private final int RED_CENTER_X = 680;
+    private final int RED_CHEF_Y = 270;
+    private final int RED_MONEY_Y = 340;
     
     public SettingsWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(960, 640, 1);
+        
         background = new GreenfootImage("bg_settings.png");
-        background.scale(background.getWidth() * 2, background.getHeight() * 2 );
+        background.scale(background.getWidth() * 5/2, background.getHeight() * 5/2 );
         setBackground(background);
+        
+        blueMenu = new GreenfootImage(140, 100);
+        
         addObjectsToWorld();
     }
     
     private void addObjectsToWorld(){
-        addObject(new Button("plus_1.png", "plus_2.png"), 300, 300);
+        // BLUE RESTAURANT BUTTONS
+        // Buttons for chefs
+        chefMinusBlue = new Button("minus_1.png", "minus_2.png");
+        chefMinusBlue.scale(2);
+        chefPlusBlue = new Button("plus_1.png", "plus_2.png");
+        chefPlusBlue.scale(2);
+        addObject(chefMinusBlue, BLUE_CENTER_X - 40, BLUE_CHEF_Y);
+        addObject(chefPlusBlue, BLUE_CENTER_X + 40, BLUE_CHEF_Y);
+        
+        // Buttons for money
+        moneyMinusBlue = new Button("minus_1.png", "minus_2.png");
+        moneyMinusBlue.scale(2);
+        moneyPlusBlue = new Button("plus_1.png", "plus_2.png");
+        moneyPlusBlue.scale(2);
+        addObject(moneyMinusBlue, BLUE_CENTER_X - 60, BLUE_MONEY_Y);
+        addObject(moneyPlusBlue, BLUE_CENTER_X + 60, BLUE_MONEY_Y);
+        
+        // RED RESTAURANT BUTTONS
+        // Buttons for chefs
+        chefMinusRed = new Button("minus_1.png", "minus_2.png");
+        chefMinusRed.scale(2);
+        chefPlusRed = new Button("plus_1.png", "plus_2.png");
+        chefPlusRed.scale(2);
+        addObject(chefMinusRed, RED_CENTER_X - 40, RED_CHEF_Y);
+        addObject(chefPlusRed, RED_CENTER_X + 40, RED_CHEF_Y);
+        
+        // Buttons for money
+        moneyMinusRed = new Button("minus_1.png", "minus_2.png");
+        moneyMinusRed.scale(2);
+        moneyPlusRed = new Button("plus_1.png", "plus_2.png");
+        moneyPlusRed.scale(2);
+        addObject(moneyMinusRed, RED_CENTER_X - 60, RED_MONEY_Y);
+        addObject(moneyPlusRed, RED_CENTER_X + 60, RED_MONEY_Y);
+        
+        playBtn = new Button("play_1.png", "play_2.png");
+        playBtn.scale(2);
+        addObject(playBtn, 480, 500);
+        
+        updateDisplay();
     }
     
+    public void act() {
+        checkButtons();
+    }
+    
+    private void checkButtons()
+    {
+        if (chefMinusBlue.isClicked() && numChefsBlue > MIN_CHEFS)
+        {
+            numChefsBlue--;
+            updateDisplay();
+        }
+        if (chefPlusBlue.isClicked() && numChefsBlue < MAX_CHEFS)
+        {
+            numChefsBlue++;
+            updateDisplay();
+        }
+        
+        if (moneyMinusBlue.isClicked() && startMoneyBlue > MIN_MONEY)
+        {
+            startMoneyBlue -= MONEY_INCREMENT;
+            updateDisplay();
+        }
+        if (moneyPlusBlue.isClicked() && startMoneyBlue < MAX_MONEY)
+        {
+            startMoneyBlue += MONEY_INCREMENT;
+            updateDisplay();
+        }
+        
+        if (chefMinusRed.isClicked() && numChefsRed > MIN_CHEFS)
+        {
+            numChefsRed--;
+            updateDisplay();
+        }
+        if (chefPlusRed.isClicked() && numChefsRed < MAX_CHEFS)
+        {
+            numChefsRed++;
+            updateDisplay();
+        }
+        
+        if (moneyMinusRed.isClicked() && startMoneyRed > MIN_MONEY)
+        {
+            startMoneyRed -= MONEY_INCREMENT;
+            updateDisplay();
+        }
+        if (moneyPlusRed.isClicked() && startMoneyRed < MAX_MONEY)
+        {
+            startMoneyRed += MONEY_INCREMENT;
+            updateDisplay();
+        }
+        
+        
+        if (playBtn.isClicked())
+        {
+            startGame();
+        }
+    }
+    
+    private void updateDisplay() {
+        // Get the background image
+        setBackground(new GreenfootImage(background));
+        GreenfootImage bg = getBackground();
+        
+        bg.setColor(new Color(99, 66, 66));
+        
+        // Draw title
+        bg.setFont(new Font("Consolas", true, false, 64));
+        bg.drawString("SETUP", 400, 150);
+        
+        // BLUE RESTAURANT (LEFT SIDE)
+        bg.setFont(new Font("Consolas", true, false, 32));
+        bg.setColor(new Color(50, 100, 200)); 
+        bg.drawString("BLUE", BLUE_CENTER_X - 50, 200);
+        
+        bg.setColor(new Color(99, 66, 66));
+        bg.setFont(new Font("Consolas", false, false, 24));
+        bg.drawString("Chefs", BLUE_CENTER_X - 70, BLUE_CHEF_Y - 20);
+        
+        bg.setFont(new Font("Consolas", true, false, 28));
+        bg.drawString(String.valueOf(numChefsBlue), BLUE_CENTER_X - 5, BLUE_CHEF_Y + 5);
+        
+        bg.setFont(new Font("Consolas", false, false, 24));
+        bg.drawString("Money", BLUE_CENTER_X - 70, BLUE_MONEY_Y - 20);
+        
+        bg.setFont(new Font("Consolas", true, false, 28));
+        bg.drawString("$" + startMoneyBlue, BLUE_CENTER_X - 40, BLUE_MONEY_Y + 5);
+        
+        // RED RESTAURANT (RIGHT SIDE)
+        bg.setFont(new Font("Consolas", true, false, 32));
+        bg.setColor(new Color(200, 50, 50)); 
+        bg.drawString("RED", RED_CENTER_X - 40, 200);
+        
+        bg.setColor(new Color(99, 66, 66));
+        bg.setFont(new Font("Consolas", false, false, 24));
+        bg.drawString("Chefs", RED_CENTER_X - 70, RED_CHEF_Y - 20);
+        
+        bg.setFont(new Font("Consolas", true, false, 28));
+        bg.drawString(String.valueOf(numChefsRed), RED_CENTER_X - 5, RED_CHEF_Y + 5);
+        
+        bg.setFont(new Font("Consolas", false, false, 24));
+        bg.drawString("Money", RED_CENTER_X - 70, RED_MONEY_Y - 20);
+        
+        bg.setFont(new Font("Consolas", true, false, 28));
+        bg.drawString("$" + startMoneyRed, RED_CENTER_X - 40, RED_MONEY_Y + 5);
+    }
+    
+    private void startGame()
+    {
+        
+    }
 }
