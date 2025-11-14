2af2642e2d5090c0f93ae41a73697a663d4c78dd
senaG1 <161520172+senaG1@users.noreply.github.com>
2025-11-14 10:46:32 -0500

    made sound manager class

---
+++ SoundManager.java
@@ -0,0 +1,25 @@
+import greenfoot.*;
+/**
+ * Write a description of class SoundManager here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SoundManager  
+{
+    // Arrays to store multiple copies of each sound
+    private static GreenfootSound[] sound1;
+    private static GreenfootSound[] sound2;
+    
+    // Indices to track which copy to play next
+    private static int sound1Index = 0;
+    private static int sound2Index = 0;
+
+    /**
+     * Initialize all sound arrays with multiple copies.
+     */
+    public static void initializeSounds()
+    {
+    }
+
+}
