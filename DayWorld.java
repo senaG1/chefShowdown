938da4e9d14f7dc6b4bba8b9e03d25edda40c8d0
isabelpowell57-cell <isabelpowell57@gmail.com>
2025-11-15 21:34:32 -0500

    Cash and rating is fixed, no longer resets

--- DayWorld.java
+++ DayWorld.java
@@ -8,10 +8,10 @@ import java.util.ArrayList;
  */
 public class DayWorld extends World
 {
-    private int day;
     private int timer = 0;
     private GreenfootImage background;
     private Font dayFont;
+    private RestaurantWorld restWorld;
     private int blueCash;
     private int redCash;
     
@@ -19,12 +19,14 @@ public class DayWorld extends World
      * Constructor for objects of class DayWorld.
      * 
      */
-    public DayWorld(int day)
+    public DayWorld(RestaurantWorld restWorld)
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(960, 640, 1);
-        this.day = day;
+        this.restWorld = restWorld;
         this.timer = 0;
+        
+        restWorld.tomorrow();
         
         getBackground().setColor(Color.BLACK);
         getBackground().fill();
@@ -32,16 +34,17 @@ public class DayWorld extends World
         getBackground().setColor(Color.WHITE);        
         dayFont = new Font ("Comic Sans MS", false, false, 100);
         getBackground().setFont(dayFont); 
-        getBackground().drawString("Day " + day, 360, 320);
+        getBackground().drawString("Day " + restWorld.getCurrentDay(), 360, 320);
     }
     
     public void act(){
         timer++;
         
         if(timer == 150){
-            Greenfoot.setWorld(new RestaurantWorld(day));
+            Greenfoot.setWorld(restWorld);
         }
     }
+    
     
     
 }
