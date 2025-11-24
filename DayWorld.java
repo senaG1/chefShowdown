81b40f36557c7371ec8e06a90f7dab16b365d4ba
33de9r <chenjiayu2033@gmail.com>
2025-11-24 10:19:07 -0500

    Update DayWorld.java

--- DayWorld.java
+++ DayWorld.java
@@ -21,6 +21,9 @@ public class DayWorld extends World
     private double blueRating;
     private int redCash;
     private double redRating;
+    
+    private Restaurant restaurantBlue;
+    private Restaurant restaurantRed;
     /**
      * Constructor for DayWorld - creates a new DayWorld.
      * This is called from Restaurant World.
@@ -44,6 +47,10 @@ public class DayWorld extends World
         this.redCash = redCash;
         this.redRating = redRating;
         
+        this.restaurantBlue = restWorld.getBlueRestaurant();
+        this.restaurantRed = restWorld.getRedRestaurant();
+
+        
         SoundManager.stopAllSounds();
         restWorld.tomorrow();
         
@@ -51,21 +58,25 @@ public class DayWorld extends World
         getBackground().fill();
         
         getBackground().setColor(Color.WHITE);        
-        dayFont = new Font ("Times New Roman", true, false, 100);
-        teamFont = new Font("Times New Roman", true, false, 60);
+        dayFont = new Font ("Times New Roman", true, false, 80);
+        teamFont = new Font("Times New Roman", true, false, 50);
         statFont = new Font("Times New Roman", false, false, 35);
         getBackground().setFont(dayFont); 
         getBackground().drawString("Day " + restWorld.getCurrentDay(), 357, 120);
         getBackground().setFont(teamFont);
-        getBackground().drawString("Blue Team:", 90, 80);
-        getBackground().drawString("Red Team: ", 580, 80);
+        getBackground().drawString("Blue Team:", 120, 100);
+        getBackground().drawString("Red Team: ", 640, 100);
         getBackground().setFont(statFont);
-        getBackground().drawString("Current Cash: $" + blueCash, 95, 160);
-        getBackground().drawString("Current Cash: $" + redCash, 575, 160);
-        getBackground().drawString("Current Rating: " + blueRating + " Stars", 95, 220);
-        getBackground().drawString("Current Rating: " + redRating + " Stars", 575, 220);
+        getBackground().drawString("Current Rating: " + blueRating + " Stars", 95, 200);
+        getBackground().drawString("Current Rating: " + redRating + " Stars", 575, 200);
         
-        addObject(new HireChef(300, blueCash), 95, 300);
+        addObject(new HireMasterChef(300, blueCash, restaurantBlue), 146, 319);
+        addObject(new HireLazyChef(200, blueCash, restaurantBlue), 348, 319);
+        addObject(new HireHungryChef(100, blueCash, restaurantBlue), 258, 523);
+        
+        addObject(new HireMasterChef(300, redCash, restaurantRed), 599, 319);
+        addObject(new HireLazyChef(200, redCash, restaurantRed), 786, 319);
+        addObject(new HireHungryChef(100, redCash, restaurantRed), 704, 523);
         //addObject(new CelebEndorsement(redCash), 575, 250);
         SoundManager.playNextDay();
     }
@@ -77,5 +88,8 @@ public class DayWorld extends World
             Greenfoot.setWorld(restWorld);
             SoundManager.playBackground();
         }
+        
+        getBackground().drawString("Current Cash: $" + blueCash, 95, 140);
+        getBackground().drawString("Current Cash: $" + redCash, 575, 140);
     }
 }
